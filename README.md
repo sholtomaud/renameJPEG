12171fbce5910a3e440f69db2e77d5ff30f08620
Sholto Maud <sholto.maud@gmail.com>
2014-07-11 12:53:50 +1000

    first

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # VERSION
 
-    version 0.0.1
+version 0.0.1
 
 # SYNOPSIS
 
-    A simple script using Hydstra pl tools to rename the extensions of image documents from jpeg to jpg.
+A simple script using Hydstra pl tools to rename the extensions of image documents from jpeg to jpg.
 
 # AUTHOR
 
-    Mr Sholto Maud <sholto.maud@gmail.com>    
-    
+Mr Sholto Maud <sholto.maud@gmail.com>    
--- renameJPG.pl
+++ renameJPG.pl
@@ -11,15 +11,15 @@ require 'hydtim.pl';
 
 =head1 VERSION
 
-    version 0.0.1
+version 0.0.1
 
 =head1 SYNOPSIS
 
-    A simple script using Hydstra pl tools to rename the extensions of image documents from jpeg to jpg.
+A simple script using Hydstra pl tools to rename the extensions of image documents from jpeg to jpg.
 
 =head1 AUTHOR
 
-    Mr Sholto Maud <sholto.maud@gmail.com>    
+Mr Sholto Maud <sholto.maud@gmail.com>    
     
 =cut
 
@@ -29,23 +29,15 @@ main: {
    
   my @documents = GetFileList('\.jp*','',0,'S:\\Hydstra\\temp\\baseline assessments\\AGECONSULTANTS\\2014-07-08');
   
-  
-  Prt('-P',HashDump(\@documents)."\n"); 
-  
   foreach ( @documents ){
     my @doc = split('\.',$_);
     
     my $extension = $doc[1];
     
-    Prt('-P',"extension [$extension]");
-    
-    $extension = ( $extension =~ m{^jp}i ) ? 'jpg':$extension ;
-    
     my $newdoc = ( $extension =~ m{^jp}i ) ? $doc[0].'.jpg': $doc[0].'.'.$extension;
     
-    Prt('-P',"olddoc [$_] newdoc [$newdoc]");
-    
     rename ($_ , $newdoc);
+    Prt('-S',"newdoc [$newdoc]                         \r");
     
   }
 }
